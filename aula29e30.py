ed07ef64e39e6bb73d970ea7677250b2b44b1ec0
Lucas Oliveira <lima1301lucas@gmail.com>
2025-01-16 16:47:53 -0300

    Seção 3 - Aulas 27 - 37

---
+++ aula29e30.py
@@ -0,0 +1,6 @@
+nome = "Lucas"
+altura = 1.85
+peso = 110
+imc = peso / (altura * altura)
+
+print(nome + " tem " + str(altura) + "de altura, \npesa " + str(peso) + " quilos e seu IMC é\n" + str(imc))
